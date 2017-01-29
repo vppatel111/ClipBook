b2a947521f221d46061a9cc51d9c0fdca09732b8
Curtis <cgoud@ualberta.ca>
2017-01-28 22:58:57 -0700

    implementation

--- MainGUI.py
+++ MainGUI.py
@@ -1,19 +1,75 @@
 from tkinter import * # import everything from tkinter
-import pyHook, pythoncom, ctypes
+import pyHook, pythoncom, ctypes, pyperclip
 import sys
 from pyHook import HookManager, GetKeyState, HookConstants
+import list_creation,read_new_list
+global book
+def retrieving(numPressed):
+    if numPressed != 10: #numPressed is a global variable that is equal to what number is pressed
+        pyperclip.copy(pastamasta[book][numPressed - 1])##does not work
+        numPressed = 10
+    return
+
+def saving(numPressed):
+    if numPressed != 10: #numPressed is a global variable that is equal to what number is pressed
+        pastamasta[book][numPressed - 1] = pyperclip.paste() ##does not work
+        print(pastamasta)
+        numPressed = 10
+    return
+
+def createNewList():
+    f = open('flameo.txt', 'r+')
+
+    for pasta in range(10):
+        pastas.append('')
+
+    for i in range(5):
+        pastamasta.append(pastas)
+
+    return
+
+def fileSave():
+    listName = 'flameo.txt'
+    f = open(listName,'w')
+    for pasta in range(len(pastas)):
+        f.write(pastas[pasta] + ' ')
+    f.close()
+    return
+
+def readNewList():
+    f.close() #if run into errors, look at this
+    global pastas
+
+    #listName = input()    ask for what the list is called
+    listName = 'test'    #for the sake of the function
+    listName = listName + '.txt'
+
+    f = open(listName, 'r+')
+    testString = f.read()
+    pastas = testString.split()
+
+    return
+
 
 def checkPress(event):
-    print(event.Key)
+    global copy, save, numPressed
     l_ctrl_press = GetKeyState(HookConstants.VKeyToID('VK_CONTROL'))
     l_alt_press = GetKeyState(HookConstants.VKeyToID('VK_MENU'))
     l_shift_press = GetKeyState(HookConstants.VKeyToID('VK_SHIFT'))
     if event.Ascii in range(48,58):
         if l_ctrl_press and l_shift_press:
-            numPressed = getNum(event)
+            numPressed = int(getNum(event))
+            retrieving(numPressed)
         if l_alt_press and l_shift_press:
-            numPressed = getNum(event)
+            numPressed = int(getNum(event))
+            saving(numPressed)
+            fileSave()
     return True
+
+def getNum(event):
+    print(event.Key)
+    return event.Key
+
 # GUI Class
 class MainGUI:
     def __init__(self, master):
@@ -39,6 +95,12 @@ class MainGUI:
 
 
 
+pastamasta  = []
+pastas = []
+
+createNewList()
+print(pastamasta)
+book = 0
 root = Tk()
 main_GUI = MainGUI(root) # Create a GUI object and run it
 hm = pyHook.HookManager()
--- retrieving.py
+++ retrieving.py
@@ -1,5 +1,5 @@
-def retrieving():
-    global numPressed, ctrlAlt, pastas
+def retrieving(numPressed):
+    global ctrlAlt, pastas
     while ctrlAlt: #ctrlAlt is a global variable that is true if ctrl and alt both pressed
         if numPressed != 10: #numPressed is a global variable that is equal to what number is pressed
             pyperclip.copy(pastas[numPressed - 1])
