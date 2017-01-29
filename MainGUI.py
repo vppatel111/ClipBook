62d6c2780218ac466df9ffc49d4f62af97e04243
Curtis <cgoud@ualberta.ca>
2017-01-28 19:48:20 -0700

    Key + GUI implementation

--- MainGUI.py
+++ MainGUI.py
@@ -1,5 +1,23 @@
 from tkinter import * # import everything from tkinter
+import pyHook, pythoncom, ctypes
+import sys
+from pyHook import HookManager, GetKeyState, HookConstants
 
+def checkPress(event):
+    print(event.Key)
+    l_ctrl_press = GetKeyState(HookConstants.VKeyToID('VK_CONTROL'))
+    l_alt_press = GetKeyState(HookConstants.VKeyToID('VK_MENU'))
+    l_shift_press = GetKeyState(HookConstants.VKeyToID('VK_SHIFT'))
+    if event.Ascii in range(48,58):
+        if l_ctrl_press and l_shift_press:
+            numPessed = getNum(event)
+            sys.exit("Stopped") ##TODO: NOT STOP
+
+        if l_alt_press and l_shift_press:
+            numPressed = getNum(event)
+            sys.exit("Stopped") ##TODO: NOT STOP
+
+    return True
 # GUI Class
 class MainGUI:
     def __init__(self, master):
@@ -27,4 +45,7 @@ class MainGUI:
 
 root = Tk()
 main_GUI = MainGUI(root) # Create a GUI object and run it
+hm = pyHook.HookManager()
+hm.KeyDown = checkPress
+hm.HookKeyboard()
 root.mainloop()
---
+++ keyPress.py
@@ -0,0 +1,40 @@
+import pyHook, pythoncom, ctypes
+import sys
+from pyHook import HookManager, GetKeyState, HookConstants
+def checkPress(event):
+    l_ctrl_press = GetKeyState(HookConstants.VKeyToID('VK_CONTROL'))
+    l_alt_press = GetKeyState(HookConstants.VKeyToID('VK_MENU'))
+    l_shift_press = GetKeyState(HookConstants.VKeyToID('VK_SHIFT'))
+    if event.Key == 'F':
+        ctypes.windll.user32.PostQuitMessage(0)##QUITS ON F KEY
+
+    if event.Ascii in range(48,58):
+        if l_ctrl_press and l_shift_press:
+            numPessed = getNum(event)
+            sys.exit("Stopped") ##TODO: NOT STOP
+
+        if l_alt_press and l_shift_press:
+            numPressed = getNum(event)
+            sys.exit("Stopped") ##TODO: NOT STOP
+
+    return True
+
+def getNum(event):
+    print(event.Key)
+    return event.Key
+
+
+hm = pyHook.HookManager()
+hm.KeyDown = checkPress
+hm.HookKeyboard()
+pythoncom.PumpMessages()
+
+#LOOPS and listens for global input
+#Recieves notification of global input
+startThread()
+print("Broken") ##WILL PRINT AFTER F
+while 1:
+    print("A")
+
+
+    time.sleep(5)
