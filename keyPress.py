3caecfbebf2014eaf67818dc2b1b5e0ce7b33dfb
Curtis <cgoud@ualberta.ca>
2017-01-28 17:03:32 -0700

    Added a break command
    Press F to break out of the event listener

--- keyPress.py
+++ keyPress.py
@@ -1,11 +1,13 @@
-import pyHook, pythoncom
+import pyHook, pythoncom, ctypes
 import sys
 from pyHook import HookManager, GetKeyState, HookConstants
-numPressed = 10
 def checkPress(event):
     l_ctrl_press = GetKeyState(HookConstants.VKeyToID('VK_CONTROL'))
     l_alt_press = GetKeyState(HookConstants.VKeyToID('VK_MENU'))
     l_shift_press = GetKeyState(HookConstants.VKeyToID('VK_SHIFT'))
+    if event.Key == 'F':
+        ctypes.windll.user32.PostQuitMessage(0)##QUITS ON F KEY
+
     if event.Ascii in range(48,58):
         if l_ctrl_press and l_shift_press:
             numPessed = getNum(event)
@@ -13,7 +15,8 @@ def checkPress(event):
 
         if l_alt_press and l_shift_press:
             numPressed = getNum(event)
-            sys.exit("Stopped")
+            sys.exit("Stopped") ##TODO: NOT STOP
+
     return True
 
 def getNum(event):
@@ -26,3 +29,4 @@ hm = pyHook.HookManager()
 hm.KeyDown = checkPress
 hm.HookKeyboard()
 pythoncom.PumpMessages()
+print("Broken") ##WILL PRINT AFTER F
